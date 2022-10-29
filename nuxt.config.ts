ba5fab2946756e709f4670faa9c032783fd798c6
anhduml <106327867+anhduml@users.noreply.github.com>
2022-10-29 10:38:20 +0700

    Nuxt.02

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    "~/assets/css/tailwind.css"
+  ],
+})
